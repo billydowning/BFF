2e04069432d4d50e5c2cb9fded4245b38f653e0d
Billy Downing <wldowning@gmail.com>
2019-02-26 18:21:15 +0100

    destroyed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,6 +22,12 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to user_booking_path(current_user, @booking)
+  end
+
   private
 
   def booking_params
