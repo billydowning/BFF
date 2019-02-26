2f23e8b10ae2689f3e95824de5c5188ab7cb5407
Deniz Bayraktaroglu <deniz.bayraktaroglu1@gmail.com>
2019-02-26 11:49:19 +0100

    added validatations to fan group model

--- app/models/fan_group.rb
+++ app/models/fan_group.rb
@@ -1,3 +1,8 @@
 class FanGroup < ApplicationRecord
   has_many :bookings
+
+
+  validates :group_name, uniqueness: true, presence: true
+  validates :category, presence: true
+  validates :price, presence: true
 end
