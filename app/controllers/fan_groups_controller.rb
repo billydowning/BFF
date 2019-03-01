7809d856ab24d5559c0bbfcfbf023121a3844628
Joost Dyckerhoff <joost.dy@web.de>
2019-03-01 14:38:57 +0100

    boss code

--- app/controllers/fan_groups_controller.rb
+++ app/controllers/fan_groups_controller.rb
@@ -1,4 +1,7 @@
 class FanGroupsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:index, :show]
+
+
   # def index
   #   @fan_groups = FanGroup.all
   #   unless params[:city].blank?
@@ -15,12 +18,40 @@ class FanGroupsController < ApplicationController
   # end
 
     def index
-      @fan_groups
-      @fan_groups = FanGroup.where("city ILIKE :city", city: params[:city]) if params[:city]
-      @fan_groups = @fan_groups.where("price <= :price", price: params[:price]) if params[:price]
-      @fan_groups = @fan_groups.where("category ILIKE :category", category: params[:category]) if params[:category] != ''
-      @fan_groups = @fan_groups.where("size <= :size", size: params[:size]) if params[:size] != ''
-    end
+      if params[:city].blank? && params[:category].blank? && params[:price].blank? && params[:size].blank?
+          @fan_groups = FanGroup.all
+      elsif params[:city].present? && params[:category].blank? && params[:price].blank? && params[:size].blank?
+        @fan_groups = FanGroup.where("city ILIKE :city", city: params[:city])
+
+      elsif params[:city].present? && params[:category].present? && params[:price].blank? && params[:size].blank?
+        @fan_groups = FanGroup.where("category ILIKE :category AND city ILIKE :city ", category: params[:category], city: params[:city])
+
+      elsif params[:city].present? && params[:category].present? && params[:price].present? && params[:size].blank?
+        @fan_groups = FanGroup.where("price <= :price AND category ILIKE :category AND city ILIKE :city ", price: params[:price], category: params[:category], city: params[:city])
+
+      elsif params[:city].present? && params[:category].present? && params[:price].present? && params[:size].present?
+        @fan_groups = FanGroup.where("price <= :price AND category ILIKE :category AND city ILIKE :city AND size <= :size", price: params[:price], category: params[:category], city: params[:city], size: params[:size])
+
+      elsif params[:city].present? && params[:category].blank? && params[:price].blank? && params[:size].present?
+        @fan_groups = FanGroup.where("city ILIKE :city AND size <= :size", city: params[:city], size: params[:size])
+
+      elsif params[:city].blank? && params[:category].blank? && params[:price].present? && params[:size].blank?
+        @fan_groups = FanGroup.where("price <= :price", price: params[:price])
+
+      else params[:city].present? && params[:category].blank? && params[:price].present? && params[:size].blank?
+        @fan_groups = FanGroup.where("price <= :price AND city ILIKE :city", price: params[:price], city: params[:city])
+
+      end
+
+
+
+
+
+      # @fan_groups = @fan_groups.where("city ILIKE :city", city: params[:city]) if params[:city] != ''
+      # @fan_groups = @fan_groups.where("price <= :price", price: params[:price]) if params[:price] != (nil || '')
+      # @fan_groups = @fan_groups.where("category ILIKE :category", category: params[:category]) if params[:category] != nil
+      # @fan_groups = @fan_groups.where("size <= :size", size: params[:size]) if params[:size] != nil
+end
 
 
   #   unless params[:city].blank?
